55f9d8a9e6772f5731160b7be06d1c1ad9174cb0
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-03-03 11:06:51 +0200

    initilize models and views directory for hms app

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HMS App",
+    'description': "HMS Application",
+    'aurthor': 'Abdelrahman Shaaban',
+    'category': "Services",
+    'version': "1.0.0.1.0",
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        # 'views/ticket.xml',
+        # # 'security/ir.model.access.csv',
+        # 'views/base_menus.xml',
+    ]
+}
