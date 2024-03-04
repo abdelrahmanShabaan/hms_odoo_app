2471fb6ba895703a0fe3bb46ee40680df33667ac
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-03-05 00:58:55 +0200

    finish project & finish reports

--- __manifest__.py
+++ __manifest__.py
@@ -11,10 +11,11 @@
         'security/groups.xml',
         'security/ir.model.access.csv',
         'views/patient/hms_patient.xml',
-        'views/patient/patient_menus.xml',
+        'views/Menu/patient_menus.xml',
         'views/department/hms_department.xml',
         'views/Doctors/hms_doctors.xml',
         'views/LogHistory/hms_logging.xml',
         'views/Customer/crm_customer_view.xml',
+        'reports/patient_report.xml',
     ]
 }
